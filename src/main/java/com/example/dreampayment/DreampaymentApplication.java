5b8d5c073ecd2dddea5d2c675a9da84d73aa321a
TeeCreates <99111124+TeeCreates@users.noreply.github.com>
2024-05-26 19:12:04 -0400

    spring initializer

---
+++ src/main/java/com/example/dreampayment/DreampaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.dreampayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreampaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreampaymentApplication.class, args);
+	}
+
+}
